48502ced112160e655e9cf5f3e4bf087d48b8afe
ZeroBug <404.zerobug@gmail.com>
2022-04-14 00:58:55 +0600

    set up the sign up

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,20 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
-    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [confirmPass, setConfirmPass] = useState('')
+    const [error, setError] = useState('')
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
+    const navigate = useNavigate();
 
+    const handleEmailBlur = (e) => {
+      setEmail(e.target.value)
+      console.log(e.target.value);
+    }
+
+    const handlePassBlur = (e) => { 
+      setPassword(e.target.value)
+    }
+
+    const handleConfirmPassBlur = (e) => { 
+      setConfirmPass(e.target.value)
+    }
+
+    if (user) {
+      navigate('/shop');
+    }
+
+    const handleCreateUser = (e) => {
+      e.preventDefault();
+      if (password !== confirmPass) {
+        setError('Your Password Does not Match')
+        return;
+      }
+
+      createUserWithEmailAndPassword(email, password);
+    }
+ 
     return (
         <div className="form-container">
       <div>
         <h2>Sign Up</h2>
-        <form>
+        <form onSubmit={handleCreateUser}>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" required/>
+          <input onBlur={handleEmailBlur} type="email" name="email" required/>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" required/>
+          <input onBlur={handlePassBlur} type="password" name="password" required/>
           <label htmlFor="confirm-password">Confirm Password</label>
-          <input type="password" name="confirm-password" required/>
+          <input onBlur={handleConfirmPassBlur}  type="password" name="confirm-password" required/>
+          <p style={{color: 'red', margin: 0}}>{error}</p>
           <br />
           <button className='login-btn'>Sign Up</button>
           <p>
